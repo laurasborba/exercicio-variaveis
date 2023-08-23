e4c37f710eea9ca73f52e010da379d8ccdf0b5dd
Laura Santos <142456003+laurasborba@users.noreply.github.com>
2023-08-22 21:45:39 -0300

    Create exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,43 @@
+/* 
+* Laura Santos
+*
+*
+*/
+
+/*1. resposta
+
+10
+10 5
+
+*/
+
+
+/*2. resposta
+
+10 10 10
+*/
+
+
+/*3. resposta
+
+
+let valorTrabalho = prompt("Quantas horas você trabalha por dia?");
+let valorDia = prompt("Quanto você recebe por dia?");
+alert(`Voce recebe ${valorDia/valorTrabalho} por hora`);
+
+*/
+
+
+/* Exercícios de escrita de código
+construa um programa
+*/
+
+const nome = "Laura";
+const idade = "25";
+
+console.log(nome);
+console.log(idade);
+
+
+
+
